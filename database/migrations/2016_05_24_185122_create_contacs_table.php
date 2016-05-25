655b7f807907a3e906e05c6211d602c3ebd07abb
infotronika <infotronika@infotronika.eu>
2016-05-25 14:49:01 +0200

    Database katalog

---
+++ database/migrations/2016_05_24_185122_create_contacs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContacsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+			$table->integer('person_id')->index();
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('contacts');
+    }
+}
